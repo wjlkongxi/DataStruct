d8a8e5f5027e3cb91165e407855ed6a7f8f8250c
wjlkongxi <2935750944@qq.com>
2019-11-03 15:16:56 +0800

    更改代码

---
+++ DataStruct/DataStruct/bintree.h
@@ -0,0 +1,433 @@
+#ifndef _BINTREE_H_
+#define _BINTREE_H_
+#include"common.h"
+//ABC##DE##F##G#H##
+typedef struct BinTreeNode
+{
+	DataType data;
+	struct BinTreeNode *leftChild;
+	struct BinTreeNode *rightChild;
+}BinTreeNode;
+typedef struct BinTree
+{
+	BinTreeNode *root;
+}BinTree;
+//二叉树创建
+void BinTreeInit(BinTree *t);
+void _BinTreeCreate_1(BinTreeNode **t);
+BinTreeNode* _BinTreeCreate_2();
+void BinTreeCreate(BinTree *t);
+BinTreeNode* _BinTreeCreateByStr(char *str, int *i);
+void BinTreeCreateByStr(BinTree *t, char *str, int *i);
+BinTreeNode* _BinTreeCreateBy_VLR_LVR(char *vlr, char *lvr, int n);
+void BinTreeCreateBy_VLR_LVR(BinTree *t, char *vlr, char *lvr, int n);
+BinTreeNode* _BinTreeCreateBy_LVR_LRV(char *vlr, char *lvr, int n);
+void BinTreeCreateBy_LVR_LRV(BinTree *t, char *vlr, char *lvr, int n);
+//二叉树遍历 递归
+void _PreOrder(BinTreeNode *t);
+void PreOrder(BinTree *t);
+void _InOrder(BinTreeNode *t);
+void InOrder(BinTree *t);
+void _PostOrder(BinTreeNode *t);
+void PostOrder(BinTree *t);
+void _LevelOrder(BinTreeNode *t);
+void LevelOrder(BinTree *t);
+//二叉树遍历 非递归
+void _PreOrderNoR(BinTreeNode *t);
+void PreOrderNoR(BinTree *t);
+void _InOrderNoR(BinTreeNode *t);
+void InOrderNoR(BinTree *t);
+void _PostOrderNoR(BinTreeNode *t);
+void PostOrderNoR(BinTree *t);
+//二叉树实现
+size_t _Size(BinTreeNode* t);
+size_t Size(BinTree *t);
+size_t _Height(BinTreeNode* t);
+size_t Height(BinTree *t);
+BinTreeNode* _Find(BinTreeNode *t, DataType key);
+BinTreeNode* Find(BinTree *t, DataType key);
+BinTreeNode* _Parent(BinTreeNode *t, DataType key);
+BinTreeNode* Parent(BinTree *t, DataType key);
+BinTreeNode* _Clone(BinTreeNode *t);
+void Clone(BinTree *t1, BinTree *t2);
+bool _Equal(BinTreeNode *t1, BinTreeNode *t2);// true false
+bool Equal(BinTree *t1, BinTree *t2);// true false
+void BinTreeInit(BinTree *t)
+{
+	t->root = NULL;
+}
+void BinTreeCreate(BinTree *t)
+{
+	//_BinTreeCreate(&t->root);
+	t->root = _BinTreeCreate_2();
+}
+void _BinTreeCreate_1(BinTreeNode **t)
+{
+	DataType item;
+	scanf("%c", &item);
+	if (item == '#')
+	{
+		*t = NULL;
+	}
+	else
+	{
+		*t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+		assert(*t != NULL);
+		(*t)->data = item;
+		_BinTreeCreate_1(&((*t)->leftChild));
+		_BinTreeCreate_1(&((*t)->rightChild));
+	}
+}
+BinTreeNode* _BinTreeCreate_2()
+{
+	DataType item;
+	scanf("%c", &item);
+	if (item == '#')
+	{
+		return NULL;
+	}
+	else
+	{
+		BinTreeNode *t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+		assert(t != NULL);
+		t->data = item;
+		t->leftChild = _BinTreeCreate_2();
+		t->rightChild = _BinTreeCreate_2();
+		return t;
+	}
+}
+void BinTreeCreateByStr(BinTree *t, char *str, int *i)
+{
+	t->root = _BinTreeCreateByStr(str, i);
+}
+BinTreeNode* _BinTreeCreateByStr(char *str, int *i)
+{
+	if (str[*i] == '#' || str[*i] == '\0')
+		return NULL;
+	else
+	{
+		BinTreeNode *t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+		t->data = str[*i];
+		(*i)++;
+		t->leftChild = _BinTreeCreateByStr(str, i);
+		(*i)++;
+		t->rightChild = _BinTreeCreateByStr(str, i);
+		return t;
+	}
+}
+BinTreeNode* _BinTreeCreateBy_VLR_LVR(char *vlr, char *lvr, int n)
+{
+	if (n == 0)
+		return NULL;
+	int k = 0;
+	while (lvr[k] != vlr[0])
+		k++;
+	BinTreeNode *t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+	t->data = lvr[k];
+	t->leftChild = _BinTreeCreateBy_VLR_LVR(vlr + 1, lvr, k);
+	t->rightChild = _BinTreeCreateBy_VLR_LVR(vlr + k + 1, lvr + k + 1, n - k - 1);
+	return t;
+}
+void BinTreeCreateBy_VLR_LVR(BinTree *t, char *vlr, char *lvr, int n)
+{
+	t->root = _BinTreeCreateBy_VLR_LVR(vlr, lvr, n);
+}
+BinTreeNode* _BinTreeCreateBy_LVR_LRV(char *lvr, char *lrv, int n)
+{
+	if (n == 0)
+		return NULL;
+	int k = 0;
+	while (lvr[k] != lrv[n - 1])
+		k++;
+	BinTreeNode *t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+	t->data = lvr[k];
+	t->rightChild = _BinTreeCreateBy_LVR_LRV(lvr + k + 1, lrv + k, n - k - 1);
+	t->leftChild = _BinTreeCreateBy_LVR_LRV(lvr, lrv, k);
+	return t;
+}
+void BinTreeCreateBy_LVR_LRV(BinTree *t, char *lvr, char *lrv, int n)
+{
+	t->root = _BinTreeCreateBy_LVR_LRV(lvr, lrv, n);
+}
+void PreOrder(BinTree *t)
+{
+	_PreOrder(t->root);
+}
+void _PreOrder(BinTreeNode *t)
+{
+	if (t != NULL)
+	{
+		printf("%c ", t->data);
+		_PreOrder(t->leftChild);
+		_PreOrder(t->rightChild);
+	}
+}
+void InOrder(BinTree *t)
+{
+	_InOrder(t->root);
+}
+void _InOrder(BinTreeNode *t)
+{
+	if (t != NULL)
+	{
+		_InOrder(t->leftChild);
+		printf("%c ", t->data);
+		_InOrder(t->rightChild);
+	}
+}
+void PostOrder(BinTree *t)
+{
+	_PostOrder(t->root);
+}
+void _PostOrder(BinTreeNode *t)
+{
+	if (t != NULL)
+	{
+		_PostOrder(t->leftChild);
+		_PostOrder(t->rightChild);
+		printf("%c ", t->data);
+	}
+}
+#include"queue.h"
+void LevelOrder(BinTree *t)
+{
+	_LevelOrder(t->root);
+}
+void _LevelOrder(BinTreeNode *t)
+{
+	if (t != NULL)
+	{
+		LinkQueue Q;
+		LinkQueueInit(&Q);
+		LinkQueueEn(&Q, t);
+		while (!LinkQueueEmpty(&Q))
+		{
+			BinTreeNode *p = LinkQueueFront(&Q);
+			LinkQueueDe(&Q);
+			printf("%c ", p->data);
+			if (p->leftChild != NULL)
+				LinkQueueEn(&Q, p->leftChild);
+			if (p->rightChild != NULL)
+				LinkQueueEn(&Q, p->rightChild);
+		}
+	}
+}
+size_t Size(BinTree *t)
+{
+	return _Size(t->root);
+}
+size_t _Size(BinTreeNode* t)
+{
+	if (t == NULL)
+		return 0;
+	else
+		return _Size(t->leftChild) + _Size(t->rightChild) + 1;
+}
+size_t Height(BinTree *t)
+{
+	return _Height(t->root);
+}
+size_t _Height(BinTreeNode* t)
+{
+	if (t == NULL)
+		return 0;
+	else
+	{
+		int left_h = _Height(t->leftChild);
+		int right_h = _Height(t->rightChild);
+		return (left_h>right_h ? left_h : right_h) + 1;
+	}
+}
+BinTreeNode* Find(BinTree *t, DataType key)
+{
+	return _Find(t->root, key);
+}
+BinTreeNode* _Find(BinTreeNode *t, DataType key)
+{
+	BinTreeNode *p;
+	if (t == NULL)
+		return NULL;
+	if (t->data == key)
+		return t;
+	p = _Find(t->leftChild, key);
+	if (p != NULL)
+		return p;
+	return _Find(t->rightChild, key);
+}
+BinTreeNode* Parent(BinTree *t, DataType key)
+{
+	return _Parent(t->root, key);
+}
+BinTreeNode* _Parent(BinTreeNode *t, DataType key)
+{
+	BinTreeNode *pr;
+	BinTreeNode *p = _Find(t, key);
+	if (t == NULL || p == NULL || p == t)
+		return NULL;
+	if (t->leftChild == p || t->rightChild == p)
+		return t;
+	pr = _Parent(t->leftChild, key);
+	if (pr != NULL)
+		return pr;
+	return _Parent(t->rightChild, key);
+}
+void Clone(BinTree *t1, BinTree *t2)
+{
+	t2->root = _Clone(t1->root);
+}
+BinTreeNode* _Clone(BinTreeNode *t)
+{
+	if (t == NULL)
+		return NULL;
+	else
+	{
+		BinTreeNode *p = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+		p->data = t->data;
+		p->leftChild = _Clone(t->leftChild);
+		p->rightChild = _Clone(t->rightChild);
+		return p;
+	}
+}
+bool Equal(BinTree *t1, BinTree *t2)
+{
+	return _Equal(t1->root, t2->root);
+}
+bool _Equal(BinTreeNode *t1, BinTreeNode *t2)
+{
+	if (t1 == NULL && t2 == NULL)
+		return true;
+	if (t1 == NULL || t2 == NULL)
+		return false;
+	return (t1->data == t2->data)
+		&& _Equal(t1->leftChild, t2->leftChild)
+		&& _Equal(t1->rightChild, t2->rightChild);
+}
+#include"stack.h"
+void PreOrderNoR(BinTree *t)
+{
+	_PreOrderNoR(t->root);
+}
+void _PreOrderNoR(BinTreeNode *t)
+{
+	if (t != NULL)
+	{
+		BinTreeNode *p;
+		ListStack st;
+		ListStackInit(&st);
+		ListStackPush(&st, t);
+		while (!ListStackEmpty(&st))
+		{
+			p = ListStackTop(&st);
+			ListStackPop(&st);
+			printf("%c ", p->data);
+			if (p->rightChild != NULL)
+				ListStackPush(&st, p->rightChild);
+			if (p->leftChild != NULL)
+				ListStackPush(&st, p->leftChild);
+		}
+	}
+}
+void InOrderNoR(BinTree *t)
+{
+	_InOrderNoR(t->root);
+}
+void _InOrderNoR(BinTreeNode *t)
+{
+	if (t != NULL)
+	{
+		BinTreeNode *p;
+		ListStack st;
+		ListStackInit(&st);
+		do
+		{
+			while (t != NULL)
+			{
+				ListStackPush(&st, t);
+				t = t->leftChild;
+			}
+			p = ListStackTop(&st);
+			ListStackPop(&st);
+			printf("%c ", p->data);
+			if (p->rightChild != NULL)
+				t = p->rightChild;
+		} while (!ListStackEmpty(&st) || t != NULL);
+	}
+}
+void PostOrderNoR(BinTree *t)
+{
+	_PostOrderNoR(t->root);
+}
+void _PostOrderNoR(BinTreeNode *t)
+{
+	if (t != NULL)
+	{
+		BinTreeNode *p, *pre = NULL;
+		ListStack st;
+		ListStackInit(&st);
+		do
+		{
+			while (t != NULL)
+			{
+				ListStackPush(&st, t);
+				t = t->leftChild;
+			}
+			p = ListStackTop(&st);
+			if (p->rightChild == NULL || p->rightChild == pre)
+			{
+				ListStackPop(&st);
+				printf("%c ", p->data);
+				pre = p;
+			}
+			else
+			{
+				t = p->rightChild;
+			}
+		} while (!ListStackEmpty(&st));
+	}
+}
+/*
+typedef BinTreeNode* BinTree;
+void BinTreeInit(BinTree *t);
+void BinTreeCreate_1(BinTree *t);
+BinTreeNode* BinTreeCreate_2();
+void BinTreeInit(BinTree *t)
+{
+*t = NULL;
+}
+void BinTreeCreate_1(BinTree *t)
+{
+DataType item;
+scanf("%c", &item);
+if(item == '#')
+{
+*t = NULL;
+}
+else
+{
+*t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+assert(*t != NULL);
+(*t)->data = item;
+BinTreeCreate_1(&((*t)->leftChild));
+BinTreeCreate_1(&((*t)->rightChild));
+}
+}
+BinTreeNode* BinTreeCreate_2()
+{
+DataType item;
+scanf("%c", &item);
+if(item == '#')
+{
+return NULL;
+}
+else
+{
+BinTreeNode *t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+assert(t != NULL);
+t->data = item;
+t->leftChild = BinTreeCreate_2();
+t->rightChild = BinTreeCreate_2();
+return t;
+}
+}
+*/
+#endif /* _BINTREE_H_ */
---
+++ DataStruct/DataStruct/test.c
@@ -0,0 +1,342 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"seqlist.h"
+//#include"slist.h"
+//#include"sclist.h"
+//#include"dlist.h"
+//#include"dclist.h"
+//#include<vld.h>
+//#include"stack.h"
+//#include"queue.h"
+//#include"bintree.h"
+#include"heap.h"
+void main()
+{
+	int ar[] = { 27, 15, 19, 18, 28, 34, 65, 49, 25, 37 };
+	int n = sizeof(ar) / sizeof(int);
+	Heap hp;
+	int i;
+	HeapInit(&hp, n);
+	for (i = 0; i<n; ++i)
+	{
+		HeapInsert(&hp, ar[i]);
+	}
+	HeapShow(&hp);
+	HeapRemove(&hp);
+	HeapDestroy(&hp);
+}
+/*
+int main()
+{
+//char *str = "ABC##DE##F##G#H##";
+//char *str1 = "ABC##D#F##GE##H##";
+char *vlr = "ABCDEFGH";
+char *lvr = "CBEDFAGH";
+char *lrv = "CEFDBHGA";
+int n = strlen(lvr);
+int i = 0, j = 0;
+BinTree bt,bt1;
+BinTreeInit(&bt);
+BinTreeInit(&bt1);
+//BinTreeCreate(&bt);
+//BinTreeCreateByStr(&bt, str, &i);
+//BinTreeCreateByStr(&bt1, str1, &j);
+//BinTreeCreateBy_VLR_LVR(&bt, vlr, lvr, n);
+BinTreeCreateBy_LVR_LRV(&bt, lvr, lrv, n);
+printf("VLR   :");
+PreOrder(&bt);
+printf("\n");
+printf("VLR NR:");
+PreOrderNoR(&bt);
+printf("\n");
+printf("\n");
+printf("LVR   :");
+InOrder(&bt);
+printf("\n");
+printf("LVR NR:");
+InOrderNoR(&bt);
+printf("\n");
+printf("\n");
+printf("LRV   :");
+PostOrder(&bt);
+printf("\n");
+printf("LRV NR:");
+PostOrderNoR(&bt);
+printf("\n");
+printf("\n");
+printf("Level:");
+LevelOrder(&bt);
+printf("\n");
+printf("Size = %d\n", Size(&bt));
+printf("Hegith = %d\n", Height(&bt));
+BinTreeNode *p = Find(&bt, 'B');
+p = Parent(&bt, 'H');
+//Clone(&bt, &bt1);
+bool flag = Equal(&bt, &bt1);
+return 0;
+}
+/*
+int main()
+{
+LinkQueue LQ;
+LinkQueueInit(&LQ);
+LinkQueueEn(&LQ, 1);
+LinkQueueEn(&LQ, 2);
+LinkQueueEn(&LQ, 3);
+LinkQueueEn(&LQ, 4);
+LinkQueueEn(&LQ, 5);
+LinkQueueShow(&LQ);
+LinkQueueDestroy(&LQ);
+return 0;
+}
+/*
+int main()
+{
+SeqCyQueue CQ;
+SeqQueueCyInit(&CQ, 8);
+SeqQueueCyEn(&CQ, 1);
+SeqQueueCyEn(&CQ, 2);
+SeqQueueCyEn(&CQ, 3);
+SeqQueueCyEn(&CQ, 4);
+SeqQueueCyEn(&CQ, 5);
+SeqQueueCyShow(&CQ);
+SeqQueueCyDe(&CQ);
+SeqQueueCyDe(&CQ);
+SeqQueueCyShow(&CQ);
+SeqQueueCyEn(&CQ, 6);
+SeqQueueCyEn(&CQ, 7);
+SeqQueueCyEn(&CQ, 8);
+SeqQueueCyEn(&CQ, 9);
+SeqQueueCyShow(&CQ);
+SeqQueueCyEn(&CQ, 19);
+return 0;
+}
+/*
+int main()
+{
+SeqQueue Q;//
+SeqQueueInit(&Q, DEFAULT_QUEUE_SIZE);
+SeqQueueEn(&Q, 1); //
+SeqQueueEn(&Q, 2);
+SeqQueueEn(&Q, 3);
+SeqQueueEn(&Q, 4);
+SeqQueueEn(&Q, 5);
+SeqQueueShow(&Q);
+SeqQueueDe(&Q);
+SeqQueueDe(&Q);
+SeqQueueShow(&Q);
+SeqQueueEn(&Q, 6);
+SeqQueueEn(&Q, 7);
+SeqQueueEn(&Q, 8);
+SeqQueueEn(&Q, 9);
+SeqQueueDestroy(&Q);
+return 0;
+}
+/*
+int main()
+{
+ListStack st;
+ListStackInit(&st);
+ListStackPush(&st, 1);
+ListStackPush(&st, 2);
+ListStackPush(&st, 3);
+ListStackShow(st);
+printf("==============\n");
+ListStackPop(&st);
+ListStackShow(st);
+return 0;
+}
+/*
+int main()
+{
+SeqStack st;
+SeqStackInit(&st, STACK_DEFAULT_SIZE);
+//SeqStack *pst = SeqStackCreate(STACK_DEFAULT_SIZE);
+SeqStackPush(&st, 1);
+SeqStackPush(&st, 2);
+SeqStackPush(&st, 3);
+SeqStackPush(&st, 4);
+SeqStackPush(&st, 5);
+SeqStackPush(&st, 6);
+SeqStackPush(&st, 7);
+SeqStackPush(&st, 8);
+int val = SeqStackTop(&st);
+SeqStackPop(&st);
+SeqStackPush(&st, 9);
+SeqStackShow(&st);
+return  0;
+}
+/*
+typedef struct Node
+{
+int data;
+struct Node *next;
+}Node;
+typedef Node* List;
+void InitList(List *head)
+{
+*head = (Node*)malloc(sizeof(Node));
+(*head)->next = NULL;
+}
+void CreateList(List *head)
+{
+Node *p = *head;
+for(int i=1; i<=5; ++i)
+{
+Node *s = (Node*)malloc(sizeof(Node));
+assert(s != NULL);
+s->data = i;
+s->next = NULL;
+p->next = s;
+p = s;
+}
+}
+struct Node* middleNode(struct Node* head)
+{
+int nodeCount = 0;
+struct Node *p = head->next;
+while(p != NULL)
+{
+nodeCount++;
+p = p->next;
+}
+p = head->next;
+for(int i=0; i<nodeCount/2; ++i)
+p = p->next;
+return p;
+}
+int main()  //主函数  主线程
+{
+List mylist;
+InitList(&mylist);
+CreateList(&mylist);
+struct Node *p = middleNode(mylist);
+return 0;
+}
+/*
+int main()
+{
+//SeqList mylist;
+//SeqListInit(&mylist, SEQLIST_DEFAULT_SIZE);
+//SList mylist;
+//SListInit(&mylist);
+//SCList mylist;
+//SCListInit(&mylist);
+//DList mylist;
+//DListInit(&mylist);
+DCList mylist;
+DCListInit(&mylist);
+DCListNode *p;
+int pos;
+int select = 1;
+DataType item, key;
+DataType ret;
+bool flag;
+while(select)
+{
+printf("*****************************************\n");
+printf("* [1] push_back         [2] push_front  *\n");
+printf("* [3] show_list         [0] quit_system *\n");
+printf("* [4] pop_back          [5] pop_front   *\n");
+printf("* [6] insert_pos        [7] insert_val  *\n");
+printf("* [8] delete_pos        [9] delete_val  *\n");
+printf("* [10] find_pos         [11] find_val   *\n");
+printf("* [12] sort             [13] reverse    *\n");
+printf("* [14] length           [15] clear      *\n");
+printf("* [16] modify_pos       [17] modify_val *\n");
+printf("* [18] capacity                         *\n");
+printf("*****************************************\n");
+printf("请选择:>");
+scanf("%d", &select);
+if(select == 0)
+break;
+switch(select)
+{
+case 1:
+printf("请输入要插入的数据<-1结束>:>");
+while(scanf("%d", &item), item!=-1)
+{
+DCListPushBack(&mylist, item);
+}
+break;
+case 2:
+printf("请输入要插入的数据<-1结束>:>");
+while(scanf("%d", &item), item!=-1)
+{
+DCListPushFront(&mylist, item);
+}
+break;
+case 3:
+DCListShow(&mylist);
+break;
+case 4:
+DCListPopBack(&mylist);
+break;
+case 5:
+DCListPopFront(&mylist);
+break;
+case 6:
+printf("请输入要插入的位置:>");
+scanf("%d", &pos);
+printf("请输入要插入的数据:>");
+scanf("%d", &item);
+//SeqListInsertByPos(&mylist, pos, item);
+break;
+case 7:
+printf("请输入要插入的数据:>");
+scanf("%d", &item);
+DCListInsertByVal(&mylist,item);
+break;
+case 8:
+printf("请输入要删除的位置:>");
+scanf("%d", &pos);
+//SeqListDeleteByPos(&mylist, pos);
+break;
+case 9:
+printf("请输入要删除的数据:>");
+scanf("%d", &key);
+DCListDeleteByVal(&mylist, key);
+break;
+case 10:
+printf("请输入要查找的位置:>");
+scanf("%d", &pos);
+//flag = SeqListFindByPos(&mylist, pos, &ret); //true false
+if(flag)
+printf("查找的数据为:> %d\n", ret);
+else
+printf("查找的数据不存在.\n");
+break;
+case 11:
+printf("请输入要查找的值:>");
+scanf("%d", &key);
+p = DCListFindByVal(&mylist, key);
+if(pos == -1)
+printf("查找的数据不存在.\n");
+else
+printf("数据所在的下标为:> %d\n", pos);
+break;
+case 12:
+DCListSort(&mylist);
+break;
+case 13:
+DCListReverse(&mylist);
+break;
+case 14:
+//printf("SeqList Length = %d\n", SListLength(&mylist));
+break;
+case 15:
+DCListClear(&mylist);
+break;
+case 18:
+//printf("capacity = %d\n", SeqListCapacity(&mylist));
+break;
+default:
+printf("输入错误，请重新输入......\n");
+break;
+}
+system("pause");
+system("cls");
+}
+DCListDestroy(&mylist);
+return 0;
+}
+*/
