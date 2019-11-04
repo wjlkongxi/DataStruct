9b7befa749d365ef5feea7178fd6d81324ce3b3c
wjlkongxi <2935750944@qq.com>
2019-11-04 23:26:50 +0800

    更新代码（6.0）

---
+++ DataStruct/DataStruct/main.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int ar[] = { 27, 15, 19, 18, 28, 34, 65, 49, 25, 37 };
+	int n = sizeof(ar) / sizeof(int);
+	void TwoWayInsertSort(n );
+		return 0;
+}
